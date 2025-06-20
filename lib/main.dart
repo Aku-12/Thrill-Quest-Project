c52ccdab75627f60e3722889002b15fb10a740e6
Aku-12 <akashchaudhary205698@gmail.com>
2025-06-20 15:47:53 +0545

    auth data source, local data source, service Locator implemented

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:thrill_quest/app.dart';
+import 'package:thrill_quest/app/app.dart';
+import 'package:thrill_quest/app/service_locator/service_locator.dart';
+import 'package:thrill_quest/core/network/hive_service.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initDependencies();
+  await HiveService().init();
   runApp(const App());
 }
