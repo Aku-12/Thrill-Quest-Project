fb6af3fb77416803f009ec9ab7cc2bf568da6240
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-03 20:01:04 +0545

    updated signup view  model

--- lib/features/auth/presentation/view_model/signup_view_model/signup_view_model.dart
+++ lib/features/auth/presentation/view_model/signup_view_model/signup_view_model.dart
@@ -1,76 +1,80 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:thrill_quest/app/service_locator/service_locator.dart';
+import 'package:thrill_quest/core/common/snackbar/my_snack_bar.dart';
 import 'package:thrill_quest/features/auth/domain/use_case/auth_register_usecase.dart';
 import 'package:thrill_quest/features/auth/presentation/view/login_view.dart';
+import 'package:thrill_quest/features/auth/presentation/view_model/login_view_model/login_view_model.dart';
 import 'package:thrill_quest/features/auth/presentation/view_model/signup_view_model/signup_event.dart';
 import 'package:thrill_quest/features/auth/presentation/view_model/signup_view_model/signup_state.dart';
 
 class SignupViewModel extends Bloc<SignupEvent, SignupState> {
- 
   final AuthRegisterUsecase _authRegisterUsecase;
- 
-  SignupViewModel(
-    this._authRegisterUsecase
-  ) : super(const SignupState()) {
-    on<RegisterEmailChanged>(_onEmailChanged);
-    on<RegisterFullNameChanged>(_onFullNameChanged);
-    on<RegisterPasswordChanged>(_onPasswordChanged);
+
+  SignupViewModel(this._authRegisterUsecase) : super(const SignupState()) {
     on<FormReset>(_onResetForm);
     on<NavigateToLoginEvent>(_onNavigateToLogin);
     on<OnSubmittedEvent>(_onSubmitted);
   }
- 
-  void _onEmailChanged(RegisterEmailChanged event, Emitter<SignupState> emit) {
-    final email = event.email;
- 
-    emit(state.copyWith(email: email));
-  }
- 
-  void _onFullNameChanged(RegisterFullNameChanged event, Emitter<SignupState> emit) {
-    final fullName = event.fullName;
- 
-    emit(state.copyWith(fullName: fullName));
-  }
- 
-  void _onPasswordChanged(RegisterPasswordChanged event, Emitter<SignupState> emit) {
-    final password = event.password;
- 
-    emit(state.copyWith(password: password));
-  }
- 
-  Future<void> _onSubmitted(OnSubmittedEvent event, Emitter<SignupState> emit) async {
-    emit(state.copyWith(emailFormStatus: EmailFormStatus.submitting));
- 
+
+  Future<void> _onSubmitted(
+    OnSubmittedEvent event,
+    Emitter<SignupState> emit,
+  ) async {
+    emit(state.copyWith(formStatus: FormStatus.submitting));
+
     final data = await _authRegisterUsecase(
       AuthRegisterParams(
+        fName: event.fName,
+        lName: event.lName,
         email: event.email,
-        fullName: event.fullName,
-        password: event.password
-      )
+        phoneNo: event.phoneNo,
+        password: event.password,
+      ),
     );
- 
-    Future.delayed(Duration(seconds: 2));
- 
+
+    await Future.delayed(const Duration(seconds: 2));
+
     data.fold(
       (l) {
-        emit(state.copyWith(emailFormStatus: EmailFormStatus.failure, message: 'Registration Failed!'));
+        emit(state.copyWith(formStatus: FormStatus.failure));
+        showMySnackBar(
+          context: event.context,
+          message: 'Account creation failed!',
+          color: Colors.red,
+        );
       },
- 
       (r) {
-        emit(state.copyWith(emailFormStatus: EmailFormStatus.success, message: 'Registration Successful!'));
-      }
-    );
-  }
- 
-  void _onResetForm(FormReset event, Emitter<SignupState> emit) {
-    emit(
-      state.copyWith(emailFormStatus: EmailFormStatus.initial, message: null),
+        emit(state.copyWith(formStatus: FormStatus.success));
+        showMySnackBar(
+          context: event.context,
+          message: 'Account successfully created!',
+          color: Colors.green,
+        );
+        add(NavigateToLoginEvent(context: event.context));
+      },
     );
   }
 
-  void _onNavigateToLogin(NavigateToLoginEvent event, Emitter<SignupState> emit) {
+  void _onResetForm(FormReset event, Emitter<SignupState> emit) {
+    emit(state.copyWith(formStatus: FormStatus.initial, message: ''));
+  }
+
+  void _onNavigateToLogin(
+    NavigateToLoginEvent event,
+    Emitter<SignupState> emit,
+  ) {
     if (event.context.mounted) {
-      Navigator.push(event.context, MaterialPageRoute(builder: (context) => LoginScreen()));
+      Navigator.push(
+        event.context,
+        MaterialPageRoute(
+          builder:
+              (context) => BlocProvider.value(
+                value: serviceLocator<LoginViewModel>(),
+                child: LoginScreen(),
+              ),
+        ),
+      );
     }
   }
 }
