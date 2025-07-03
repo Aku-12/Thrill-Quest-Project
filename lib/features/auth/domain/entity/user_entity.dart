7b744b8f59052f75e5a43a202a7a1089621808ac
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-03 19:54:37 +0545

    updated the user entity as per backend user model

--- lib/features/auth/domain/entity/user_entity.dart
+++ lib/features/auth/domain/entity/user_entity.dart
@@ -3,16 +3,22 @@ import 'package:equatable/equatable.dart';
 class UserEntity extends Equatable {
   final String? id;
   final String email;
-  final String fullName;
+  final String fName;
+  final String lName;
+  final String phoneNo;
   final String password;
- 
+  final String? role;
+
   const UserEntity({
     this.id,
     required this.email,
-    required this.fullName,
+    required this.fName,
+    required this.lName,
+    required this.phoneNo,
     required this.password,
+    this.role = 'customer',
   });
- 
+
   @override
-  List<Object?> get props => [id, email, fullName, password];
+  List<Object?> get props => [id, email, fName, lName, phoneNo, password, role];
 }
