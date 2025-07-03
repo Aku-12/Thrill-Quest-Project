aa9fe31f28edb083caae43834ffd7d9029478efc
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-03 20:11:31 +0545

    main file is updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:thrill_quest/app/app.dart';
 import 'package:thrill_quest/app/service_locator/service_locator.dart';
-import 'package:thrill_quest/core/network/hive_service.dart';
+// import 'package:thrill_quest/core/network/hive_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initDependencies();
-  await HiveService().init();
+  // await HiveService().init();
   runApp(const App());
 }
