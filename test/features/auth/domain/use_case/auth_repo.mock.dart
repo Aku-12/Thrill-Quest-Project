0509ec895066a8a4c046df25ef332f3e5199325d
Aku-12 <akashchaudhary205698@gmail.com>
2025-07-13 13:53:31 +0545

    auth repo mock test implemented

---
+++ test/features/auth/domain/use_case/auth_repo.mock.dart
@@ -0,0 +1,4 @@
+import 'package:mocktail/mocktail.dart';
+import 'package:thrill_quest/features/auth/domain/repository/auth_repository.dart';
+
+class AuthRepoMock extends Mock implements IAuthRepository {}
