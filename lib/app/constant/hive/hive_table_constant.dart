561385fc904c47ba5a6d37beda6595db1689805a
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 10:43:56 +0545

    Update Hive table constants

--- lib/app/constant/hive/hive_table_constant.dart
+++ lib/app/constant/hive/hive_table_constant.dart
@@ -1,6 +1,15 @@
 class HiveTableConstant {
   HiveTableConstant._();
- 
+
   static const int userTableId = 0;
   static const String userBox = "userBox";
+
+  static const int bookingsTableId = 1;
+  static const String bookingsBox = "bookingsBox";
+
+  static const int guidesTableId = 2;
+  static const String guidesBox = "guidesBox";
+
+  static const int favoritesTableId = 2;
+  static const String favoritesBox = "favoritesBox";
 }
