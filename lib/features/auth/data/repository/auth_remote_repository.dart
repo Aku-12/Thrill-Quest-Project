80856438debb049e28ae42db34c1a66a1702f895
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 10:45:27 +0545

    Update auth remote repository

--- lib/features/auth/data/repository/auth_remote_repository.dart
+++ lib/features/auth/data/repository/auth_remote_repository.dart
@@ -1,3 +1,4 @@
+// lib/features/auth/data/repository/auth_remote_repository.dart
 import 'package:dartz/dartz.dart';
 import 'package:thrill_quest/core/error/failure.dart';
 import 'package:thrill_quest/features/auth/data/data_source/remote_local_source/auth_remote_datasource.dart';
@@ -6,28 +7,102 @@ import 'package:thrill_quest/features/auth/domain/repository/auth_repository.dar
 
 class AuthRemoteRepository implements IAuthRepository {
   final AuthRemoteDatasource _authRemoteDatasource;
+
   AuthRemoteRepository({required AuthRemoteDatasource authRemoteDatasource})
     : _authRemoteDatasource = authRemoteDatasource;
+
   @override
   Future<Either<Failure, void>> createAccount(UserEntity user) async {
     try {
       await _authRemoteDatasource.createAccount(user);
-      return Right(null);
+      return const Right(null);
     } catch (error) {
       return Left(ApiFailure(message: error.toString()));
     }
   }
 
   @override
-  Future<Either<Failure, String>> loginToAccount(
+  Future<Either<Failure, ({UserEntity user, String token})>> loginToAccount(
     String email,
     String password,
   ) async {
     try {
-      final token = await _authRemoteDatasource.loginToAccount(email, password);
-      return Right(token);
+      final result = await _authRemoteDatasource.loginToAccount(
+        email,
+        password,
+      );
+      return Right(result);
     } catch (error) {
       return Left(ApiFailure(message: error.toString()));
     }
+  }
+
+  @override
+  Future<Either<Failure, void>> changePassword({
+    required String userId,
+    required String oldPassword,
+    required String newPassword,
+  }) async {
+    try {
+      await _authRemoteDatasource.changePassword(
+        userId: userId,
+        oldPassword: oldPassword,
+        newPassword: newPassword,
+      );
+      return const Right(null);
+    } catch (error) {
+      return Left(ApiFailure(message: error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> getUserProfile() async {
+    try {
+      final user = await _authRemoteDatasource.getUserProfile();
+      return Right(user);
+    } catch (error) {
+      return Left(ApiFailure(message: error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> updateProfile(
+    UserEntity updatedUser,
+    String? imageFilePath,
+  ) async {
+    try {
+      final user = await _authRemoteDatasource.updateProfile(
+        updatedUser,
+        imageFilePath,
+      );
+      return Right(user);
+    } catch (error) {
+      return Left(ApiFailure(message: error.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, UserEntity>> getCachedUser() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, String>> getTokenLocally() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, void>> logoutUser() {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, void>> saveTokenLocally(String token) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Either<Failure, void>> saveUserLocally(UserEntity user) {
+    throw UnimplementedError();
   }
 }
