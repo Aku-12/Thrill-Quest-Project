090773d828a067ba8601cf848c75da4b749dd288
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 10:31:58 +0545

    android manifest and activity ky updated for sensors impelementation

--- android/app/src/main/kotlin/com/dev/akash/thrill_quest/MainActivity.kt
+++ android/app/src/main/kotlin/com/dev/akash/thrill_quest/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.dev.akash.thrill_quest
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
