9d50df2300c41bf548c3e2edfc355351fc48919c
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 10:45:26 +0545

    Add auth local data source

--- lib/features/auth/data/data_source/local_data_source/auth_local_data_source.dart
+++ lib/features/auth/data/data_source/local_data_source/auth_local_data_source.dart
@@ -1,35 +1,40 @@
-// import 'package:thrill_quest/core/network/hive_service.dart';
-// import 'package:thrill_quest/features/auth/data/data_source/auth_data_source.dart';
-// import 'package:thrill_quest/features/auth/data/model/auth_hive_model.dart';
-// import 'package:thrill_quest/features/auth/domain/entity/user_entity.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:thrill_quest/core/network/hive_service.dart';
+import 'package:thrill_quest/features/auth/data/model/user_hive_model.dart';
+import 'package:thrill_quest/features/auth/domain/entity/user_entity.dart';
 
-// class AuthLocalDataSource implements IAuthDataSource {
-//   final HiveService _hiveService;
- 
-//   AuthLocalDataSource({required HiveService hiveService})
-//     : _hiveService = hiveService;
- 
-//   @override
-//   Future<void> createAccount(UserEntity user) async {
-//     try {
-//       final authHiveModel = AuthHiveModel.fromEntity(user);
-//       await _hiveService.createAccount(authHiveModel);
-//     } catch(error) {
-//       throw Exception('Registration Failed: $error');
-//     }
-//   }
-  
-//   @override
-//   Future<String> loginToAccount(String email, String password) async {
-//     try {
-//       final userData = await _hiveService.login(email, password);
-//       if (userData != null && userData.password == password) {
-//         return 'Login successful!';
-//       } else {
-//         throw Exception('Invalid email or password');
-//       }
-//     } catch(error) {
-//       throw Exception('Login failed! $error');
-//     }
-//   }
-// }
+class AuthLocalDatasource {
+  final HiveService _hiveService;
+  final FlutterSecureStorage _secureStorage;
+
+  static const String _tokenKey = 'auth_token';
+
+  AuthLocalDatasource({
+    required HiveService hiveService,
+    required FlutterSecureStorage secureStorage,
+  }) : _hiveService = hiveService,
+       _secureStorage = secureStorage;
+
+  Future<void> saveUserToHive(UserEntity user) async {
+    final userHiveModel = UserHiveModel.fromEntity(user);
+    await _hiveService.saveUser(userHiveModel);
+  }
+
+  Future<UserEntity?> getUserFromHive() async {
+    final userHiveModel = await _hiveService.getUser();
+    return userHiveModel?.toEntity();
+  }
+
+  Future<void> saveToken(String token) async {
+    await _secureStorage.write(key: _tokenKey, value: token);
+  }
+
+  Future<String?> getToken() async {
+    return await _secureStorage.read(key: _tokenKey);
+  }
+
+  Future<void> clearAllAuthData() async {
+    await _hiveService.clearAuthData();
+    await _secureStorage.delete(key: _tokenKey);
+  }
+}
