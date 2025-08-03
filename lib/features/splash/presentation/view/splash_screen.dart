35d4962e19739a70399a0e12568c33d4ab2911f4
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 10:45:28 +0545

    Update splash screen transition behavior

--- lib/features/splash/presentation/view/splash_screen.dart
+++ lib/features/splash/presentation/view/splash_screen.dart
@@ -1,22 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lottie/lottie.dart';
+import 'package:thrill_quest/features/splash/presentation/view_model/splash_event.dart';
+import 'package:thrill_quest/features/splash/presentation/view_model/splash_state.dart';
+import 'package:thrill_quest/features/splash/presentation/view_model/splash_view_model.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 4),(
-    ){
-            // ignore: use_build_context_synchronously
-            Navigator.pushReplacementNamed(context, '/login');
-
-    });
-    return Scaffold(
-      body:Padding(padding: const EdgeInsets.all(8.0),
-      child: Center(
-        child: Lottie.asset("assets/animation/trillsplash.json"),
-      ),),
+    WidgetsBinding.instance.addPostFrameCallback(
+      (_) => context.read<SplashViewModel>().add(AppStart()),
+    );
+    return BlocListener<SplashViewModel, SplashState>(
+      listener: (context, state) {
+        if (state is AuthenticatedUser) {
+          Navigator.pushReplacementNamed(context, '/dashboard');
+        } else if (state is UnAuthenticatedUser) {
+          Navigator.pushReplacementNamed(context, '/login');
+        }
+      },
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Center(
+            child: Lottie.asset("assets/animation/trillsplash.json"),
+          ),
+        ),
+      ),
     );
   }
 }
