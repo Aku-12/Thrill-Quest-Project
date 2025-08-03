9ec1714641aefc129bbed26902be18580ffb0385
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 10:45:27 +0545

    Update auth repository interface

--- lib/features/auth/domain/repository/auth_repository.dart
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -4,5 +4,24 @@ import 'package:thrill_quest/features/auth/domain/entity/user_entity.dart';
 
 abstract interface class IAuthRepository {
   Future<Either<Failure, void>> createAccount(UserEntity user);
-  Future<Either<Failure, String>> loginToAccount(String email, String password);
+  Future<Either<Failure, ({UserEntity user, String token})>> loginToAccount(
+    String email,
+    String password,
+  );
+  Future<Either<Failure, UserEntity>> getUserProfile();
+  Future<Either<Failure, void>> changePassword({
+    required String userId,
+    required String oldPassword,
+    required String newPassword,
+  });
+  Future<Either<Failure, UserEntity>> updateProfile(
+    UserEntity updatedUser,
+    String? imageFilePath,
+  );
+
+  Future<Either<Failure, UserEntity>> getCachedUser();
+  Future<Either<Failure, void>> saveUserLocally(UserEntity user);
+  Future<Either<Failure, void>> saveTokenLocally(String token);
+  Future<Either<Failure, String>> getTokenLocally();
+  Future<Either<Failure, void>> logoutUser();
 }
