89197ed11d607cb16062c16668a1d1db0a2da9a8
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 21:29:59 +0545

    Remove unused mock for token use case

--- test/features/auth/domain/use_case/token.mock.dart
+++
@@ -1,4 +0,0 @@
-import 'package:mocktail/mocktail.dart';
-import 'package:thrill_quest/app/shared_pref/token_shared_prefs.dart';
-
-class MockTokenSharedPrefs extends Mock implements TokenSharedPrefs {}
