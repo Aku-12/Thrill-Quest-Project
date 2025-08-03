ebd6c72af0733cf519356ba8ee8b9f21d0d6c9de
Aku-12 <akashchaudhary205698@gmail.com>
2025-08-03 10:43:56 +0545

    Update API endpoints constants

--- lib/app/constant/api/api_endpoints.dart
+++ lib/app/constant/api/api_endpoints.dart
@@ -1,19 +1,38 @@
 class ApiEndpoints {
   ApiEndpoints._();
- 
+
   static const connectionTimeout = Duration(seconds: 1000);
- 
+
   static const receiveTimeout = Duration(seconds: 1000);
- 
+
   static const String serverAddress = "http://10.0.2.2:5050";
- 
+
   static const String baseUrl = "$serverAddress/api";
- 
-  // static const String imageUrl = "$serverAddress/uploads";
- 
+
+  static const String imageUrl = "$serverAddress/uploads";
+
+  static const String profileUrl = "$serverAddress/";
+
   //Auth
- 
+
   static const String register = "/auth/register";
- 
+
   static const String login = "/auth/login";
+
+  static const String activities = "/admin/activities";
+
+  static const getAllGuides = '/admin/guides';
+  static const String review = '/api/review';
+
+  static const String createBookings = "/admin/bookings/create";
+  static const String updateBookings = "/update/bookings";
+  static const String getMyBookings = "/admin/bookings/my-bookings";
+  //profile
+  static const String getUserProfile = "/profile/fetch";
+  static const String changePassword = "/password/change";
+  static String updateProfile(String userId) => "/profile/update/$userId";
+
+  static const String getFavorites = "/profile/get-favorites";
+  static const String addFavorite = "/profile/add-favorite";
+  static const String removeFavorite = "/profile/remove-favorite";
 }
